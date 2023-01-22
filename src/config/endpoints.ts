62279b503685929c5d2e10f5d1cf1eec73afe29f
Kevin czaja <czajakevin@gmail.com>
2023-01-22 18:53:32 +0100

    M1-#1 basic msw architecture

---
+++ src/config/endpoints.ts
@@ -0,0 +1,15 @@
+const endpoints = {
+	base: import.meta.env.VITE_BASE_API,
+
+	cards: '/cards',
+	card: '/cards/{id}',
+	sets: '/sets',
+	set: '/set/{id}',
+	booster: '/sets/{id}/booster',
+	types: '/types',
+	subtypes: '/subtypes',
+	supertypes: '/supertypes',
+	formats: '/formats',
+};
+
+export default endpoints;
---
+++ src/services/mocks/handlers.ts
@@ -0,0 +1,28 @@
+import { rest } from "msw";
+import endpoints from "../../config/endpoints";
+
+export const handlers = [
+    rest.get(endpoints.types, (_, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                "types": [
+                    "Artifact",
+                    "Conspiracy",
+                    "Creature",
+                    'Enchantment',
+                    "Instant",
+                    "Land",
+                    "Phenomenon",
+                    "Plane",
+                    "Planeswalker",
+                    "Scheme",
+                    "Sorcery",
+                    "Tribal",
+                    "Vanguard"
+                ]
+            })
+        );
+    }),
+];
+
