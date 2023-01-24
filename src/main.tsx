5eea3b53e1de941d061b954af35d1df08bbea141
Kevin Czaja <czajakevin@gmail.com>
2023-01-24 21:31:37 +0100

    M1 #1 setup initial services architecture with msw (#11)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 
+if (import.meta.env.MODE === 'development') {
+  startMSWWorker();
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+async function startMSWWorker() {
+  const { worker } = await import('./services/mocks/browser');
+  worker.start();
+}
