9a959324330ee52416b71a0965e4c1e6662b901c
Cyril Tiger <cyriltiger@gmail.com>
2023-01-24 23:23:35 +0100

    M1-#4-Add Interface, mock localStorage and mock implementation tests

---
+++ src/test/mocks/storage.ts
@@ -0,0 +1,20 @@
+export class LocalStorageMock {
+  items: Map<string, string>;
+
+  constructor() {
+    this.items = new Map();
+  }
+
+  setItem(keyName: string, keyValue: string) {
+    this.items.set(keyName, keyValue);
+  }
+  getItem(keyName: string) {
+    return this.items.get(keyName) ?? null;
+  }
+  removeItem(keyName: string) {
+    this.items.delete(keyName);
+  }
+  clear() {
+    this.items.clear();
+  }
+}
---
+++ src/test/storage.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { LocalStorageMock } from './mocks/storage';
+
+describe('This is the mock for the local storage', () => {
+  let mockStorage: LocalStorageMock;
+
+  beforeEach(() => {
+    mockStorage = new LocalStorageMock();
+  });
+
+  it('setItem: Add a new item to storage', () => {
+    mockStorage.setItem('test', 'coucou');
+
+    expect(mockStorage.items.has('test')).toBe(true);
+    expect(mockStorage.items.get('test')).toBe('coucou');
+  });
+
+  it('setItem: Adding a new item to existing key overwrites old value', () => {
+    mockStorage.items.set('existingItem', 'oldValue');
+
+    mockStorage.setItem('existingItem', 'newValue');
+    expect(mockStorage.items.get('existingItem')).toBe('newValue');
+  });
+
+  it('getItem: Retrieve one item from storage', () => {
+    mockStorage.items.set('test', 'item');
+
+    expect(mockStorage.getItem('test')).toBe('item');
+  });
+
+  it('getItem: Returns null if key does not exist', () => {
+    expect(mockStorage.getItem('test')).toBe(null);
+  });
+
+  it('removeItem: Remove one item from storage', () => {
+    mockStorage.items.set('test', 'item');
+
+    mockStorage.removeItem('test');
+    expect(mockStorage.items.has('test')).toBe(false);
+  });
+
+  it('clear: Clean the storage', () => {
+    mockStorage.items.set('test', 'item');
+    mockStorage.items.set('test2', 'item2');
+
+    mockStorage.clear();
+    expect(mockStorage.items.size).toBe(0);
+  });
+});
