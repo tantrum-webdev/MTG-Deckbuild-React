6755f99ac8f946e70b64d709c8ee34f5408c8d8a
Cyril Tiger <cyriltiger@gmail.com>
2023-02-19 16:42:19 +0100

    M1-#7-invert queries + container queries

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { Table } from '@/components';
 import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Table />
-    </div>
-  );
+  return <Table />;
 }
 
 export default App;
