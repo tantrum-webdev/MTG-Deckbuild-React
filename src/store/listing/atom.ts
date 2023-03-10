da5ebae5266e2e8fa303243a77d46140bff16058
Cyril Tiger <cyriltiger@gmail.com>
2023-03-10 22:51:35 +0100

    cleanup

--- src/store/listing/atom.ts
+++ src/store/listing/atom.ts
@@ -2,10 +2,6 @@ import { storage } from '@/services';
 import { Deck } from '@/types';
 import { atom, AtomEffect } from 'recoil';
 
-/**
- * TODO:
- * - Check documentation to eventually clean it up / comment here to provide more info
- */
 const syncStorage: AtomEffect<Deck[]> = ({ setSelf, onSet }) => {
   setSelf(storage.index());
 
