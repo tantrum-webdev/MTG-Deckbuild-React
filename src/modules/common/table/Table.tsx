897b55c67ba35f8306e0883472a7563b8e5825e5
Kevin czaja <czajakevin@gmail.com>
2023-03-25 18:32:55 +0100

    M2-#17 implement deck removal logic into table component

--- src/modules/common/table/Table.tsx
+++ src/modules/common/table/Table.tsx
@@ -1,10 +1,15 @@
 import { deckListState } from '@/store/listing';
-import { useRecoilValue } from 'recoil';
-import RemoveDeckButton from '@/modules/common/actionButton/RemoveDeckButton';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { ActionButton } from '@/modules/common';
 import classes from './Table.module.css';
 
 export default function Table() {
   const decks = useRecoilValue(deckListState);
+  const setDeckList = useSetRecoilState(deckListState);
+
+  const removeDeck = (id: string) => {
+    setDeckList((decks) => decks.filter((deck) => deck.id !== id));
+  };
 
   return (
     <table className={classes.table}>
@@ -22,7 +27,11 @@ export default function Table() {
             <td data-label="Name">{name}</td>
             <td data-label="Format">{format}</td>
             <td data-label="Actions">
-              <RemoveDeckButton id={id} />
+              <ActionButton
+                action={() => removeDeck(id)}
+                textContent="Delete"
+                value="save"
+              />
             </td>
           </tr>
         ))}
