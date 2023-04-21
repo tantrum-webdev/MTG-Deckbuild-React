a25ee8b3cfce9c5797c6d56daded84f0505d97ce
Kevin czaja <czajakevin@gmail.com>
2023-04-22 00:55:58 +0200

    M2-#18 move methods declaration outside filteredDeckListState selector scope

--- src/store/listing/withFilters.ts
+++ src/store/listing/withFilters.ts
@@ -3,13 +3,15 @@ import { deckListState, nameFilterState, formatFilterState } from './atom';
 import { Deck } from '@/types';
 import { isEmpty } from '@/helpers';
 
+const isMatchingName = (deck: Deck, name: RegExp): boolean =>
+  !!deck.name.match(name);
+
+const isMatchingFormat = (deck: Deck, format: string): boolean =>
+  isEmpty(format) || deck.format === format;
+
 export const filteredDeckListState = selector({
   key: 'FilteredDeckList',
   get: ({ get }) => {
-    const isMatchingName = (deck: Deck, name: RegExp) => deck.name.match(name);
-    const isMatchingFormat = (deck: Deck, format: string) =>
-      isEmpty(format) || deck.format === format;
-
     const deckList = get(deckListState);
     const deckNameFilter = get(nameFilterState);
     const deckFormatFilter = get(formatFilterState);
