cd5bf11abfe7b67b0c27c477f47661ef46d1c483
Cyril Tiger <cyriltiger@gmail.com>
2023-04-23 01:09:19 +0200

    add renderListing helper method

--- src/modules/listing/Listing.test.tsx
+++ src/modules/listing/Listing.test.tsx
@@ -3,20 +3,23 @@ import Listing from './Listing';
 import { RecoilRoot } from 'recoil';
 import { render, screen } from '@testing-library/react';
 
+const renderListing = () =>
+  render(
+    <RecoilRoot>
+      <Listing />
+    </RecoilRoot>
+  );
+
 describe('Deck Listing', () => {
   vi.stubGlobal('localStorage', new LocalStorageMock());
 
-  beforeEach(() => {
+  afterEach(() => {
     localStorage.clear();
   });
 
   describe('Get Decks from storage', () => {
     it('Render empty table when there are no decks in storage', () => {
-      render(
-        <RecoilRoot>
-          <Listing />
-        </RecoilRoot>
-      );
+      renderListing();
 
       expect(screen.queryByRole('cell')).toBe(null);
     });
@@ -24,11 +27,7 @@ describe('Deck Listing', () => {
     it('Renders rows in table when there are existing decks', () => {
       localStorage.setItem('decks', JSON.stringify(decks));
 
-      render(
-        <RecoilRoot>
-          <Listing />
-        </RecoilRoot>
-      );
+      renderListing();
 
       // We add 1 since there is the header row that is automatically counter
       expect(screen.getAllByRole('row')).toHaveLength(decks.length + 1);
