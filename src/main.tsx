fdba4c03ea9915e5f4dfb2607c1a6b45583c9ac7
Cyril Tiger <cyriltiger@gmail.com>
2023-05-13 17:23:25 +0200

    add error route + basic error component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
 import App from './App';
 
 if (import.meta.env.MODE === 'development') {
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Listing } from '@/modules/listing';
+import { Error } from '@/modules/error';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Listing />,
+  },
+  {
+    path: '*',
+    element: <Error />,
   },
 ]);
